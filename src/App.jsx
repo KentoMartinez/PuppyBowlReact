7eac46c7667489a893ea56f16524c9aa2a95d6d4
KentoMartinez <ayrtonmg4@gmail.com>
2023-08-07 19:14:16 -0700

    new updates

--- src/App.jsx
+++ src/App.jsx
@@ -12,32 +12,56 @@ import FakeTabs from "./Components/FakeTabs";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 function App() {
-  const navigate = useNavigate() 
+  const navigate = useNavigate();
   return (
     <>
       <div id="container">
         <div id="navbar">
-        <Navbar  fixed="top" bg="dark" data-bs-theme="dark">
-          <Container>
-            <Nav className="me-auto my-2 my-lg-0">
-              <Navbar.Brand href="/" id="main-header">
-                PUPPY BOWL 2023
-              </Navbar.Brand>
-              <Nav.Link onClick={navigate("/teams")}>Teams</Nav.Link>
-              <Nav.Link onClick={navigate("/forms")}>Forms</Nav.Link>
-              <Nav.Link onClick={navigate("/fakelink")}>Games</Nav.Link>
-              <Nav.Link onClick={navigate("/fakelink")}>Tickets</Nav.Link>
-              <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-primary">Search</Button>
-          </Form>
-            </Nav>
-          </Container>
+          <Navbar fixed="top" bg="dark" data-bs-theme="dark">
+            <Container>
+              <Nav className="me-auto my-2 my-lg-0">
+                <Navbar.Brand href="/" id="main-header">
+                  PUPPY BOWL 2023
+                </Navbar.Brand>
+                <Nav.Link
+                  onClick={() => {
+                    navigate("/teams");
+                  }}
+                >
+                  Teams
+                </Nav.Link>
+                <Nav.Link
+                  onClick={() => {
+                    navigate("/newplayerform");
+                  }}
+                >
+                  Forms
+                </Nav.Link>
+                <Nav.Link
+                  onClick={() => {
+                    navigate("/fakelink");
+                  }}
+                >
+                  Games
+                </Nav.Link>
+                <Nav.Link
+                  onClick={() => {
+                    navigate("/fakelink");
+                  }}
+                >
+                  Tickets
+                </Nav.Link>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-primary">Search</Button>
+                </Form>
+              </Nav>
+            </Container>
           </Navbar>
         </div>
         <Routes id="main-section">
